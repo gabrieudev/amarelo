1dea2a325e162a498652e9a2e99995ec6012b8ce
João Gabriel da Conceição Santos <gs495770@gmail.com>
2024-06-10 18:31:30 -0300

    initial commit

---
+++ src/main/java/com/api/amarelo/AmareloApplication.java
@@ -0,0 +1,13 @@
+package com.api.amarelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmareloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmareloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/amarelo/AmareloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.amarelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmareloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
